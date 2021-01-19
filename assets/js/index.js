7d1858d8d791fbdae12cfe6a69b91df3136c54f2
zmj <1241311306@qq.com>
2021-01-19 21:40:36 +0800

    个人中心内容完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,27 @@
 $(function () {
-    getUserInof();
+    //1.获取用户信息
+    getUserInfo();
+    // 2.退出
+    var layer = layui.layer;
     $("#btnLogout").on("click", function () {
+        //框架提供的询问框
         layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function (index) {
             localStorage.removeItem("token");
-            location.href = "/login.html"
+            location.href = "/login.html";
 
             layer.close(index);
         });
-    })
 
+    })
 });
-function getUserInof() {
+function getUserInfo() {
     $.ajax({
         url: "/my/userinfo",
         // headers: {
-        //     Authorization: localStorage.getItem("token") || ""
+        //       Authorization: localStorage.getItem("token") || ""
         // },
         success: function (res) {
-            // console.log(res);
+            // console.log(res)
             if (res.status !== 0) {
                 return layui.layer.msg(res.message);
             }
@@ -26,15 +30,17 @@ function getUserInof() {
     })
 }
 function renderAvatar(user) {
+    // 1.渲染名称
     var name = user.nickname || user.username;
-    $('#welcome').html("欢迎&nbsp;&nbsp;" + name);
-    if (user.user_pic !== null) {
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+    if (user.uesr_pic !== null) {
+        // 有头像
         $(".layui-nav-img").show().attr("src", user.user_pic);
-        $(".text-avater").hide();
+        $(".text-avatar").hide();
     } else {
-        $('.layui-nav-img').hide();
+        // 莫得头像
+        $(".layui-nav-img").hide();
         var text = name[0].toUpperCase();
-        $('.text-avater').show().html(text);
+        $(".text-avatar").show().html(text);
     }
-
 }
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,48 @@
+$(function () {
+    //1.自定义验证规则
+    var form = layui.form;
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return "昵称长度为1-6位之间!";
+            }
+        }
+    });
+    //2.用户渲染
+    initUserInfo();
+    var layer = layui.layer;
+    function initUserInfo() {
+        $.ajax({
+            method: "GET",
+            url: '/my/userinfo',
+            success: function (res) {
+                // console.log(res)
+                if (res.status != 0) {
+                    return layer.msg(res.message);
+                }
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    //3.表单重置
+    $("#btnReset").on("click", function (e) {
+        e.preventDefault();
+        initUserInfo()
+    })
+    //4.修改用户信息
+    $('.layui-form').on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: "/my/userinfo",
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg("用户信息修改失败！")
+                }
+                layer.msg("恭喜您，用户信息修改成功！")
+                window.parent.getUserInfo();
+            }
+        })
+    })
+})
